05cf1e3404da843dba337c22ad62a5aa2b96628e
Lemon Sama <burrisc24@gmail.com>
2022-06-17 15:36:50 -0400

    side bar styling and icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './Sidebar.css'
 import logo from '../../assets/images/logo/logo2.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faFile, faHome } from '@fortawesome/free-solid-svg-icons';
 
 
 const Sidebar = () => {
     return (
-        <div className='nav-bar'>
-            <img className='logo' src={logo} alt='logo' />
+        <>
 
-        </div>
+            <header className="flex-row px-1 nav-bar">
+                <img className='logo flex-row' src={logo} alt='logo' />
+                <h2 className='flex-row'>
+                    Welcome to my Portfolio!
+                </h2>
+
+                <nav className='flex-row'>
+
+                    <ul className="flex-row">
+                        <li className="mx-2">
+                            <a data-testid="about" href="#about">
+                                <FontAwesomeIcon icon={faHome} color='#4d4de' />
+                            </a>
+                        </li>
+                        <li className="mx-2">
+                            <a data-testid="about" href="#about">
+                                <FontAwesomeIcon icon={faFile} color='#4d4de' />
+                            </a>
+                        </li>
+                        <li className="mx-2">
+                            <a data-testid="about" href="#about">
+                                <FontAwesomeIcon icon={faEnvelope} color='#4d4de' />
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </header>
+
+        </>
 
     )
 }
